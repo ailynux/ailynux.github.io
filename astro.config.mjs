50730c104c03d1cce60da61faa6acdf0a97ea51f
Ailyn Diaz <95152597+ailynux@users.noreply.github.com>
2024-09-15 00:50:51 -0600

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default defineConfig({
-  integrations: [react()],
-});
